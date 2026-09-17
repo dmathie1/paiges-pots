ee956a341e4c48e92f3e7690dbfeb521908b4293
Dylan <dmathie1@outlook.com>
2026-09-17 22:32:57 +0100

    Added lightbox effect to gallery images +highlight box to nav +pulse effect to buttons

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ const nav = document.querySelector('nav');
 toggleButton.addEventListener('click',function(){
     nav.classList.toggle('open');
 });
+
+const galleryImages = document.querySelectorAll('.piece img')
+
+const lightbox  = document.createElement('div');
+lightbox.classList.add('lightbox');
+document.body.appendChild(lightbox);
+
+const lightboxImg = document.createElement('img');
+lightbox.appendChild(lightboxImg);
+
+galleryImages.forEach(function(image){
+    image.addEventListener('click', function(){
+        lightboxImg.src = image.src;
+        lightbox.classList.add('open');
+    });
+});
+
+lightbox.addEventListener('click', function(){
+    lightbox.classList.remove('open');
+});
