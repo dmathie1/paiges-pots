625ed8b9d283af1449d7716fae59ac88ae5e26e2
Dylan <dmathie1@outlook.com>
2026-09-17 12:46:09 +0100

    + mobile hamburger nav

---
+++ script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector('.nav-toggle');
+const nav = document.querySelector('nav');
+
+toggleButton.addEventListener('click',function(){
+    nav.classList.toggle('open');
+});
